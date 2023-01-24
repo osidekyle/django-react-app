694a867e94671d51088c568a8456afebaafb0f8e
osidekyle <hawkinskyle67@gmail.com>
2023-01-23 16:16:54 -0800

    typo in urls file

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('room', RoomView.as_view()),
     path('create-room', CreateRoom.as_view()),
     path('get-room', GetRoom.as_view()),
-    path('join-room' JoinRoom.as_view())
+    path('join-room', JoinRoom.as_view())
 ]
